67c4e7eb1d9f8898db51207032c72d0a8841711d
Maximo <maxmonteiro86@gmail.com>
2023-04-14 11:52:24 -0300

    primeiro tapa

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+export default function Home() {
+  return (
+    <h2>hello world</h2>
+  )
+}
