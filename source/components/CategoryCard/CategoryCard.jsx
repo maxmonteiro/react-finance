669cc629963307a921c855d539c16f0a57a34324
Maximo <maxmonteiro86@gmail.com>
2023-04-15 11:59:34 -0300

    cria hook para formatacao da moeda

--- source/components/CategoryCard/CategoryCard.jsx
+++ source/components/CategoryCard/CategoryCard.jsx
@@ -1,14 +1,13 @@
+import useGetCurrency from '@/source/hooks/useGetCurrency';
 import styles from './CategoryCard.module.scss';
 import {TbPencil} from 'react-icons/tb';
+
 
 const CategoryCard = ({ data }) => {
 
     const { title, amount, percentage } = data;
 
-    const currency = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-    }).format(amount);
+    const formatedAmount = useGetCurrency(amount);
 
     return (
         <div className={styles.card}>
@@ -16,7 +15,7 @@ const CategoryCard = ({ data }) => {
                 <TbPencil className='icon hover'/>
             </div>
             <h2>{title}</h2>
-            <h3>{currency}</h3>
+            <h3>{formatedAmount}</h3>
             <h4>Alocado: {percentage}%</h4>
         </div>
     )
