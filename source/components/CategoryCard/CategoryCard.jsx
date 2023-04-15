b7501df18406178fe9b8348dbcb8f9c8087a51b3
Maximo <maxmonteiro86@gmail.com>
2023-04-15 11:55:26 -0300

    formata o valor da moeda

--- source/components/CategoryCard/CategoryCard.jsx
+++ source/components/CategoryCard/CategoryCard.jsx
@@ -5,13 +5,18 @@ const CategoryCard = ({ data }) => {
 
     const { title, amount, percentage } = data;
 
+    const currency = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(amount);
+
     return (
         <div className={styles.card}>
             <div className={styles.edit}>
                 <TbPencil className='icon hover'/>
             </div>
             <h2>{title}</h2>
-            <h3>{amount}</h3>
+            <h3>{currency}</h3>
             <h4>Alocado: {percentage}%</h4>
         </div>
     )
