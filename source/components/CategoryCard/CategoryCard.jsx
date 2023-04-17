4071cf564d3b9afa96ea96a2d7d74bf2efc5e663
Maximo <maxmonteiro86@gmail.com>
2023-04-17 15:41:19 -0300

    deixa exibicao do menu do card dinamica

--- source/components/CategoryCard/CategoryCard.jsx
+++ source/components/CategoryCard/CategoryCard.jsx
@@ -2,19 +2,23 @@ import useGetCurrency from '@/source/hooks/useGetCurrency';
 import styles from './CategoryCard.module.scss';
 import {TbPencil} from 'react-icons/tb';
 import CategoryMenu from './CategoryMenu';
+import { useState } from 'react';
 
 
 const CategoryCard = ({ data }) => {
 
     const { title, amount, percentage } = data;
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleMenuHandler = () => setIsVisible(!isVisible);
 
     const formatedAmount = useGetCurrency(amount);
 
     return (
         <div className={styles.card}>
-            <div className={styles.edit}>
+            <div className={styles.edit} onClick={toggleMenuHandler}>
                 <TbPencil className='icon hover'/>
-                <CategoryMenu />
+                {isVisible ? <CategoryMenu /> : null}
             </div>
             <h2>{title}</h2>
             <h3>{formatedAmount}</h3>
